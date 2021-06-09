d2a5675776e2e642fe1abad252dff0b9736dac73
madiyar <hi@madiyar.dev>
2021-06-09 23:24:16 +0600

    store

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { useLocalStorage } from './hooks';
 import { Main, Edit } from './pages';
 
+export const AppContext = React.createContext({});
+
 const App = () => {
+  const [store, setStore] = useLocalStorage<any[]>('store', []);
+
+  const deleteResume = (id : string) => {
+    setStore(store.filter(cv => cv.id !== id));
+  };
+
+  const getResume = (id: string) => {
+    return store.find(cv => cv.id === id)
+  };
+
+  const saveResume = (data: any) => {
+    setStore(store.map(cv => cv.id === data.id ? ({ ...cv, ...data }) : cv))
+  };
+
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route path="/edit/:id" component={Edit} />
-        <Route path="*" render={() => <Redirect to="/" />} />
-      </Switch>
-    </BrowserRouter>
+    <AppContext.Provider value={{ store, deleteResume, getResume, setStore, saveResume }}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/edit/:id" component={Edit} />
+          <Route path="*" render={() => <Redirect to="/" />} />
+        </Switch>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 };
 
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Card, Dimmer, Image, Container, Grid, Header, Button } from 'semantic-ui-react'
+import { AppContext } from '../../App';
 
 const image = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOcOnHifwAGMgK4RoxAtgAAAABJRU5ErkJggg==";
 
@@ -8,7 +10,7 @@ const Resume = () => {
 
   const content = (
     <>
-      <Button primary fluid style={{ marginBottom: 8 }}>Edit</Button>
+      <Button as={Link} to="/edit/1" primary fluid style={{ marginBottom: 8 }}>Edit</Button>
       <Button color="red" fluid>Delete</Button>
     </>
   );
@@ -32,22 +34,25 @@ const Resume = () => {
   )
 };
 
-const Main = () => (
-  <Container>
-    <Grid columns='equal' padded>
-      <Grid.Row verticalAlign="middle">
-        <Grid.Column>
-          <Header as="h1">Resumes</Header>
-        </Grid.Column>
-        <Grid.Column textAlign="right">
-          <Button content="Create New" primary icon="add" labelPosition="left" size="large" />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-    <Card.Group itemsPerRow={4} stackable style={{ marginTop: 16 }}>
-      {[1,2,3,4,5,6].map(i => <Resume key={i} />)}
-    </Card.Group>
-  </Container>
-);
+const Main = () => {
+  const { store, setStore } = useContext<any>(AppContext);
+  return (
+    <Container>
+      <Grid columns='equal' padded>
+        <Grid.Row verticalAlign="middle">
+          <Grid.Column>
+            <Header as="h1">Resumes</Header>
+          </Grid.Column>
+          <Grid.Column textAlign="right">
+            <Button content="Create New" primary icon="add" labelPosition="left" size="large" onClick={() => setStore([...store, 123])} />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <Card.Group itemsPerRow={4} stackable style={{ marginTop: 16 }}>
+        {store.map((_:any, i:any) => <Resume key={i} />)}
+      </Card.Group>
+    </Container>
+  );
+};
 
 export default Main;
